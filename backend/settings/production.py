be8a7a23714d665a2ad1a207f616993de3bfdd15
Eugene <limzhiweieugene@gmail.com>
2018-04-03 19:59:49 -0400

    Update production database

--- backend/settings/production.py
+++ backend/settings/production.py
@@ -1,8 +1,12 @@
 from backend.settings.common import *
 import os
+import dj_database_url
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
 DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com']
+
+db_from_env = dj_database_url.config(conn_max_age=500, require_ssl=True)
+DATABASES['default'].update(db_from_env)
