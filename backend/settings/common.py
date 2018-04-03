b7eb5d61e14821f05d6dbb55ece55999a2495659
Eugene <limzhiweieugene@gmail.com>
2018-04-03 19:31:16 -0400

    Remove whitenoise

--- backend/settings/common.py
+++ backend/settings/common.py
@@ -127,5 +127,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(
     BASE_DIR, '..', 'frontend', 'build', 'static').replace('\\', '/'), ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
